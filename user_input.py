afbc2daf8edcba98bce7f12b62d16b1dadd7f011
ibrahim-kiri <ibrahimkiri@gmail.com>
2024-11-01 18:51:39 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,8 @@
+# Ask for user details
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+print(f"\nHello, {name}!")
+print(f"You are {age} years old and live in {location}.")
+print(f"It's great to meet you, {name}!")
